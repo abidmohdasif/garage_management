add4f4c6efc48858cba569e28bd42a2418701a79
abidmohdasif <abidmohdasif@gmail.com>
2026-04-23 08:29:16 -0400

    added garge to test and added test for success

--- test_garage.py
+++ test_garage.py
@@ -1,9 +1,18 @@
 import pytest
 from garage import calculate_fee, enter_garage
 
+pytest.fixture
+def garage = {
+    "cars": {}, # car_id -> entry_hour (int)
+    "capacity": 10   # total number of spots        
+}
 def test_enter_garage_Type_Error():
     with pytest.raises(TypeError):
         enter_garage("garage1", 12345, "hello")
+
+def test_enter_garage_success(garage):
+    enter_garage(garage,12345,9)
+    assert garage["cars"] == {12345 : 9}
 
 def test_calculate_fee():
     assert calculate_fee(3,2) == 6.00
