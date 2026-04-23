251c7783ea5e54841e01b903377e457590c9fb0b
abidmohdasif <abidmohdasif@gmail.com>
2026-04-23 08:12:44 -0400

    added implementation for calculate fee type error

--- garage.py
+++ garage.py
@@ -8,6 +8,8 @@ def get_available_spots(garage):
     pass
 
 def calculate_fee(hours, rate):
+    if not isinstance(hours, (int,float)) or not isinstance(rate,(int,float)):
+        raise TypeError("hours or rate is not int/float")
     if hours < 0 or rate < 0:
         raise ValueError("hours or rate cannot be negative")
     return round(hours * rate, 2)
