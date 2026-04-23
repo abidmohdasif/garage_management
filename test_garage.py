da5a75e5946d4239f68bd2c3be0f1ae66a026d61
abidmohdasif <abidmohdasif@gmail.com>
2026-04-23 08:40:49 -0400

    try this

--- test_garage.py
+++ test_garage.py
@@ -2,6 +2,10 @@ import pytest
 from garage import calculate_fee, enter_garage
 
 
+def test_enter_garage_success(garage1):
+     enter_garage(garage1,12345,9)
+     assert garage1["cars"] == {12345 : 9}
+     
 def test_enter_garage_Type_Error():
     with pytest.raises(TypeError):
         enter_garage("garage1", 12345, "hello")
@@ -10,9 +14,7 @@ def test_enter_garage_Value_Error_car_id():
     with pytest.raises(ValueError):
         enter_garage("garage1",12345, 3)
 
-# def test_enter_garage_success(garage1):
-#     enter_garage(garage1,12345,9)
-#     assert garage1["cars"] == {12345 : 9}
+
 
 def test_calculate_fee():
     assert calculate_fee(3,2) == 6.00
