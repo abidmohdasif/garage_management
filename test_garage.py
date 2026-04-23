fa4331b20b5adc991be659a5da5a759f20d38291
abidmohdasif <abidmohdasif@gmail.com>
2026-04-23 08:29:39 -0400

    try this should fail

--- test_garage.py
+++ test_garage.py
@@ -2,7 +2,7 @@ import pytest
 from garage import calculate_fee, enter_garage
 
 pytest.fixture
-def garage = {
+def garage {
     "cars": {}, # car_id -> entry_hour (int)
     "capacity": 10   # total number of spots        
 }
