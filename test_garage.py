d5a9e76406610479c1e849ae55dd20612bdf3919
abidmohdasif <abidmohdasif@gmail.com>
2026-04-23 08:16:54 -0400

    added dictionary to garage

--- test_garage.py
+++ test_garage.py
@@ -1,10 +1,6 @@
 import pytest
 from garage import calculate_fee
 
-# garage1 = {
-#     "capacity": 10,   # total number of spots
-#     "cars": {}         # car_id -> entry_hour (int)
-# }
 
 # def test_enter_garage_new_car():
 #     assert enter_garage(garage1, 12345, 2) ==
