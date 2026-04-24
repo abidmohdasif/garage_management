957f9f375783e6579c3e2a9ae67742be667ea7b9
abidmohdasif <abidmohdasif@gmail.com>
2026-04-24 07:43:26 -0400

    basic implementation

--- garage.py
+++ garage.py
@@ -12,7 +12,8 @@ def enter_garage(garage, car_id, entry_hour):
     garage['cars'][car_id] = entry_hour
 
 def exit_garage(garage, car_id):
-    pass
+    
+    garage['cars'].pop(car_id)
 
 def get_available_spots(garage):
     pass
