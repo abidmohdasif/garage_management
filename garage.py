9e31c1cd135d7ef0c7cddc9e65d150e151d8a3f3
abidmohdasif <abidmohdasif@gmail.com>
2026-04-24 07:51:25 -0400

    added implementation

--- garage.py
+++ garage.py
@@ -14,11 +14,11 @@ def enter_garage(garage, car_id, entry_hour):
 def exit_garage(garage, car_id):
     if car_id not in garage['cars'].keys():
         raise KeyError("Car ID is not in this garage try again")
-        
+
     garage['cars'].pop(car_id)
 
 def get_available_spots(garage):
-    pass
+    return garage["capacity"] - len(garage["cars"])
 
 def calculate_fee(hours, rate):
     if not isinstance(hours, (int,float)) or not isinstance(rate,(int,float)):
